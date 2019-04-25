0ae1cc4c8d1670c925360efce4158bc371d69e86
nyxb71 <nyxb71@gmail.com>
2019-04-24 22:08:24 -0500

    add more features to player

--- game/src/game_objects/Character.cs
+++ game/src/game_objects/Character.cs
@@ -1,13 +1,25 @@
 namespace game {
     public class Character : IGo, ILook {
         private Location loc;
+        public uint Health {get; private set;}
+        public bool Alive {get; private set;}
 
         public void Go(Direction dir) {
-
+            // attempt to exit in {dir} direction
         }
 
         public void Look(Direction dir) {
+            // look in {dir} direction
+        }
 
+        public void TakeDamage(uint dmg) {
+            if (Health >= dmg) {
+                Health -= dmg;
+            }
+            else {
+                Health = 0;
+                Alive = false;
+            }
         }
     }
 }
--- game/src/game_objects/Player.cs
+++ game/src/game_objects/Player.cs
@@ -1,7 +1,16 @@
+using System;
+
 namespace game
 {
     public class Player : Character
     {
+        public uint Experience {get; private set;}
 
+        public void AddExperience(uint xp)
+        {
+            Experience += xp;
+        }
+
+        public uint Level() => (uint)Math.Floor(Math.Sqrt(Experience)) ;
     }
 }
