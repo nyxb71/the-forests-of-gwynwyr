3f80a800abc3bd1fdff2fb1e72b5d2b82da3b01d
nyxb71 <nyxb71@gmail.com>
2019-04-26 23:06:20 -0500

    rename game1, instantiate objects within game init

--- game/src/Program.cs
+++ game/src/Program.cs
@@ -7,7 +7,7 @@ namespace game
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new ForestsGame())
                 game.Run();
         }
     }
---
+++ game/src/main/ForestGame.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace game
+{
+    public class ForestsGame : SadConsole.Game
+    {
+        private World World;
+        private List<Zone> Zones;
+        private Player Player;
+        private GameEventQueue GameEvents;
+
+        public ForestsGame() : base("", CONFIG.WIDTH, CONFIG.HEIGHT, null)
+        {
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            IsMouseVisible = false;
+            base.Initialize();
+
+            Zones = GameData.LoadZones("data/zones.json");
+            World = new World(Zones);
+            Player = new Player();
+            GameEvents = new GameEventQueue();
+
+            UserInterface.Init();
+        }
+
+        protected override void LoadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
+
+            GameEvents.ProcessNext();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
