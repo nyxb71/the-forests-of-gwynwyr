44e5340fd30d8b4ba0bea839eca201d41b056d67
nyxb71 <nyxb71@gmail.com>
2019-04-27 18:56:02 -0500

    rename, give Location a constructor

--- game/src/game_objects/DataTypes.cs
+++
@@ -1,6 +0,0 @@
-namespace game {
-    public struct Location {
-        public uint x;
-        public uint y;
-    }
-}
---
+++ game/src/game_objects/Location.cs
@@ -0,0 +1,10 @@
+namespace game {
+    public struct Location {
+        public readonly uint x, y;
+
+        public Location(uint a, uint b) {
+            (x, y) = (a, b);
+        }
+    }
+
+}
