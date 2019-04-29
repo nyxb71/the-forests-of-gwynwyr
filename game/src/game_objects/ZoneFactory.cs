82a9cd5bbf60b9e7bd8a0413a1af85b8d5b4156a
nyxb71 <nyxb71@gmail.com>
2019-04-28 20:55:10 -0500

    update look texts, zone name

--- game/src/game_objects/ZoneFactory.cs
+++ game/src/game_objects/ZoneFactory.cs
@@ -10,10 +10,10 @@ namespace game
             var zones = new List<Zone>();
 
             var default_look_texts = new Dictionary<Direction, string>() {
-                { Direction.north, "look_north" },
-                { Direction.south, "look_south" },
-                { Direction.east, "look_east" },
-                { Direction.west, "look_west" }
+                { Direction.north, "You look north" },
+                { Direction.south, "You look south" },
+                { Direction.east,  "You look east" },
+                { Direction.west,  "You look west" }
             };
 
             var default_event_texts = new Dictionary<string, string>() {
@@ -24,7 +24,7 @@ namespace game
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
                     zones.Add(new Zone(
-                        string.Format($"Zone{i},{j}"),
+                        $"Z({i},{j})",
                         new Location(i, j),
                         default_look_texts,
                         default_event_texts
